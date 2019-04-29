e2679ecc1332a7d240f34002c8a1cb0d720b4e86
Shawn Ren <shawnren527@gmail.com>
2019-04-29 18:16:00 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(params.require(:post).permit(:title, :description, :post_status)
+    @post.update(params.require(:post))
     redirect_to post_path(@post)
   end
 
