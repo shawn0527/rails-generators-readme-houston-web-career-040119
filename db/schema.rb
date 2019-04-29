43d89dd2e57a9faa9470a506251e3a3bf18bd2fe
Shawn Ren <shawnren527@gmail.com>
2019-04-29 18:13:13 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190429180000) do
+ActiveRecord::Schema.define(version: 20190429180630) do
+
+  create_table "acconts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
